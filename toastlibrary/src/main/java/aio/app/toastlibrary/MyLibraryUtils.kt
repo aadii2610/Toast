e1ee216ec05f740b7969c050a5736197150d06ca
aadii2610 <136782933+aadii2610@users.noreply.github.com>
2024-01-31 12:13:46 +0500

    yml commit

--- toastlibrary/src/main/java/aio/app/toastlibrary/MyLibraryUtils.kt
+++ toastlibrary/src/main/java/aio/app/toastlibrary/MyLibraryUtils.kt
@@ -3,7 +3,7 @@ package aio.app.toastlibrary
 import android.content.Context
 import android.widget.Toast
 
-class MyLibraryUtils {
+class MyLibraryUtils{
     fun showToast(context: Context, message: String) {
         Toast.makeText(context, ""+message, Toast.LENGTH_SHORT).show()
     }
